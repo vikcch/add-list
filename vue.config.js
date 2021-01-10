3559c67a76467be71b90cfed506f8adb6debeb82
vik <vik>
2021-01-10 17:19:43 +0000

    chore: ghpages

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,6 @@ module.exports = {
             extensions: ['.js', '.vue', '.json']
         }
     },
-    productionSourceMap: false
+    productionSourceMap: false,
+    publicPath: "/add-list/"
 }
